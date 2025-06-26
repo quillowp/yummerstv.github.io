6420a516b7303a3d5f41bc4e80dba66e25cce48b
quillowp <143898219+quillowp@users.noreply.github.com>
2025-06-26 19:39:04 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,11 @@
 const gameStart = document.getElementById("gameStart")
 const canvas = document.getElementById("game")
+const ctx = canvas.getContext("2d")
+const walls = document.getElementsByClassName("wall")
+const interactables = document.getElementsByClassName("interact")
+const collectibles = document.getElementsByClassName("collect")
+const textBox = document.getElementById("textHidden")
+const game1 = document.getElementsByClassName("game1")
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -12,17 +18,26 @@ gameStart.onclick = async function() {
     let loadInner = document.createElement("div")
     loadInner.setAttribute("id", "loadInner")
     document.getElementById("gameDiv").appendChild(load)
+    ctx.beginPath(); // Start a new path
+    ctx.rect(0, 0, 1000, 1000); // Add a rectangle to the current path
+    ctx.fill(); // Render the path
     await delay(Math.random()*(1200-200) + 600)
     load.appendChild(loadInner)
     await delay(Math.random()*(400-0) + 0)
     document.getElementById("loadInner").style.width = Math.random()*(117-77) + 77
     await delay(100)
     load.remove()
+    ctx.fillStyle = "#353535"
+    ctx.fillRect(0,0,1000,1000)
+
 
     let player = document.createElement("div")
     player.setAttribute("id", "player")
     const gameDiv = document.getElementById("gameDiv")
     document.body.insertBefore(player, gameDiv)
+    for (let i=0;i<game1.length;i++) {
+      game1.item(i).style.display = "block"
+    }
 }
 
 up = false
@@ -32,16 +47,24 @@ left = false
 
 let x = 694
 let y = 2187
-let speed = 1.5
-let diagonalSpeed = 1.1
+let speed = 2
+let diagonalSpeed = 1.5
 let defaultSpeed = speed
+let previousX
+let previousY
+let colLeft
+let colRight
+let colUp
+let colDown
+let facing = "down"
+let text = []
+let items = []
 
 document.addEventListener('keydown', (event) => {
   const keyPressed = event.key; // Gets the string representation of the key (e.g., "Enter", "a", "ArrowUp")
   const keyCode = event.keyCode; // Gets the numeric key code (deprecated, but still commonly used for older browsers)
 
-  console.log(`Key pressed: ${keyPressed}`);
-  console.log(`Key Code: ${keyCode}`);
+
    if (event.key === 'ArrowUp' || event.key === 'ArrowDown' || 
         event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
         
@@ -51,15 +74,32 @@ document.addEventListener('keydown', (event) => {
   // Example: Perform an action if the "Enter" key is pressed
   if (keyPressed === 'ArrowLeft') {
     left = true
+    facing = "left"
   }
   if (keyPressed === 'ArrowRight') {
     right = true
+    facing = "right"
   }
   if (keyPressed === 'ArrowUp') {
     up = true
+    facing = "up"
   }
   if (keyPressed === 'ArrowDown') {
     down = true
+    facing = "down"
+  }
+  if (keyPressed === 'z') {
+    if (text.length < 1) {
+      interactCheck()
+    }
+    if (text.length > 0) {
+      textBox.children.item(0).innerHTML = text[0]
+      text.shift()
+      textBox.setAttribute("id", "text")
+    }
+    if (text.length == 0) {
+      textBox.setAttribute("id", "textHidden")
+    }
   }
 
   if ((left == true || right == true) && (up == true || down == true)) {
@@ -76,8 +116,7 @@ document.addEventListener('keyup', (event) => {
   const keyPressed = event.key; // Gets the string representation of the key (e.g., "Enter", "a", "ArrowUp")
   const keyCode = event.keyCode; // Gets the numeric key code (deprecated, but still commonly used for older browsers)
 
-  console.log(`Key pressed: ${keyPressed}`);
-  console.log(`Key Code: ${keyCode}`);
+
 
   // Example: Perform an action if the "Enter" key is pressed
   if (keyPressed === 'ArrowLeft') {
@@ -92,6 +131,7 @@ document.addEventListener('keyup', (event) => {
   if (keyPressed === 'ArrowDown') {
     down = false
   }
+  
 
   if ((left == true || right == true) && (up == true || down == true)) {
     speed = diagonalSpeed
@@ -102,23 +142,201 @@ document.addEventListener('keyup', (event) => {
 
 });
 
+function collision(pixelAmountReturn) {
+  let p = document.getElementById("player")
+  for (let i = 0; i<walls.length; i++) {
+    let wall = walls[i].getBoundingClientRect()
+    let player = p.getBoundingClientRect()
+
+    if (!(wall.right < player.left || wall.left > player.right || wall.bottom < player.top || wall.top > player.bottom)) {
+      if ((wall.right > player.left) && (player.top < wall.bottom) && (player.bottom > wall.top) && (wall.right < player.right)) {
+        colLeft = true
+        colRight=false
+        colUp=false
+        colDown=false
+        console.log("left")
+        if (pixelAmountReturn == "left") {
+          return wall.right
+        }
+        return true
+      } else {
+        colLeft = false
+      }
+      if ((wall.left < player.right) && (player.top < wall.bottom) && (player.bottom > wall.top) && (wall.left > player.left)){
+        colRight = true
+        colLeft=false
+        colUp=false
+        colDown=false
+        console.log("right")
+        if (pixelAmountReturn == "right") {
+          return wall.left  - (player.right-player.left)
+        }
+        return true
+      } else {
+        colRight = false
+      }
+      if ((wall.bottom > player.top) && (player.right > wall.left) && (player.left < wall.right) && (wall.top < player.top)) {
+        colUp = true
+        colRight=false
+        colLeft=false
+        colDown=false
+        console.log("up")
+        if (pixelAmountReturn == "top") {
+          return wall.bottom +1
+        }
+        return true
+      } else {
+        colUp = false
+      }
+      if ((wall.top < player.bottom) && (player.right > wall.left) && (player.left < wall.right)  && (wall.bottom > player.bottom)) {
+        colDown = true
+        colRight=false
+        colUp=false
+        colLeft=false
+        console.log("down")
+        if (pixelAmountReturn == "bottom") {
+          return wall.top - (player.bottom-player.top) -1
+        }
+        return true
+      } else {
+        colDown = false
+      }
+      console.log("colliding")
+      return true
+    }
+  }
+  colLeft = false
+  colRight = false
+  colDown = false
+  colUp = false
+  return false
+}
+
+function collectibleCollision() {
+  let p = document.getElementById("player")
+  for (let i = 0; i<collectibles.length; i++) {
+    let collect = collectibles[i].getBoundingClientRect()
+    let player = p.getBoundingClientRect()
+
+    if (!(collect.right < player.left || collect.left > player.right || collect.bottom < player.top || collect.top > player.bottom)) {
+      items.push(collectibles[i].id)
+      collectibles[i].remove()
+    }
+}
+}
+
+function interactCheck() {
+  let p = document.getElementById("player")
+  let player = p.getBoundingClientRect()
+  let xOff
+  let yOff
+  console.log(facing)
+
+
+  if (facing == "left") {
+    xOff = -20
+    yOff = 0
+  }
+  if (facing == "right") {
+    xOff = 20
+    yOff = 0
+  }
+  if (facing == "up") {
+    xOff = 0
+    yOff = -20
+  }
+  if (facing == "down") {
+    xOff = 0
+    yOff = 20
+  }
+
+  for (let i = 0; i<interactables.length; i++) {
+    let object = interactables[i].getBoundingClientRect()
+    if (!(object.right < player.left + xOff || object.left > player.right + xOff || object.bottom < player.top + yOff || object.top > player.bottom + yOff)) {
+      if (interactables[i].hasChildNodes()) {
+        let children = interactables[i].children
+        for (let j=0; j<children.length;j++) {
+          if (children.item(j).nodeName == "P") {
+            text.push(children.item(j).innerHTML)
+          }
+          
+        }
+        text.push("last")
+      }
+
+  }
+}
+}
+
+function scrollWithPlayer() {
+  let p = document.getElementById("player")
+  let player = p.getBoundingClientRect()
+
+
+  if (player.top<320) {
+    window.scrollBy(0,-speed*2)
+  }
+  if (player.top>450) {
+    window.scrollBy(0,speed)
+  }
+}
+
 
 const gameloop = setInterval(() => {
+  previousX = x
+  previousY = y
+
+
     if (left) {
+      if (!colLeft) {
         x -= speed
+      }
     }
     if (right) {
+        if (!colRight) {
         x += speed
+      }
     }
     if (down) {
+        if (!colDown) {
         y += speed
+      }
     }
     if (up) {
+        if (!colUp) {
         y -= speed
+      }
     }
 
+    
+
     let player = document.getElementById("player")
+    
     player.style.left = x + "px"
     player.style.top = y + "px"
+    if (collision()) {
+      x = previousX
+      y = previousY
+    }
+
+      // if (colLeft) {
+      //   x = collision("left")
+      // }
+      // if (colRight) {
+      //   x = collision("right")
+      // }
+      // if (colUp) {
+      //   y = collision("top") 
+      // }
+      // if (colDown) {
+      //   y = collision("bottom") 
+      // }
+
+    
+    player.style.left = x + "px"
+    player.style.top = y + "px"
+    collectibleCollision()
+    scrollWithPlayer()
+    
     
 }, 10);
